7b67d9da63ea3e19ae0a34c0b437d6cc78f9fe89
octavianc96 <octavian.c96@gmail.com>
2017-03-13 03:51:34 +0200

    added operators functions w/o delete

--- Lab 2/src/application/MainController.java
+++ Lab 2/src/application/MainController.java
@@ -9,16 +9,67 @@ public class MainController {
 	@FXML
 	private Label display;
 	private String output = "";
+	private double firstNum;
+	private double secondNum;
+	private String operatorInput;
+	private double result;
+	private String text;
+	private boolean isDel = false;
+	
+	
+	@FXML
+	public void processDel (ActionEvent event){
+		isDel = true;
+		processNum();
+		
+	}
 	
 	@FXML
 	public void processNum (ActionEvent event) {
-		String value = ((Button)event.getSource()).getText();
-		display.setText(display.getText() + value);
+		text = ((Button)event.getSource()).getText();
+		if (text == Delete){
+			
+		}
+			
+		if(isDel == true){
+			text.substring(0, (text.length() - 1));
+			isDel = false;
+		}
+		if (display.getText() == "0")
+			display.setText("");
+
+		display.setText(display.getText() + text);
 	}
 	
 	@FXML
 	public void processOperators (ActionEvent event) {
+		double value;
+		operatorInput = ((Button)event.getSource()).getText();
+		switch (operatorInput){
+		case "C":
+			display.setText("0");
+			break;
+		case "±":
+			value = Double.parseDouble(display.getText());
+			value = value * (-1);
+			display.setText(String.valueOf(value));
+			break;
+		case "√":
+			firstNum = Double.parseDouble(display.getText());
+			result = Math.sqrt(firstNum);
+			display.setText(String.valueOf(result));
+			break;
+		case "%":
+		case "÷":
+		case "x":
+		case "-":
+		case "+":
 		
+		case "x^":
+			firstNum = Double.parseDouble(display.getText());
+			display.setText("");
+			
+		}
 	}
 	
 	@FXML
@@ -28,7 +79,34 @@ public class MainController {
 	
 	@FXML
 	public void processEqual (ActionEvent event) {
-		
+		secondNum = Double.parseDouble(display.getText());
+		switch (operatorInput){
+		case "%":
+			if (display.getText().equals(""))
+				result = 0;
+			else
+			result = firstNum * secondNum / 100;
+			break;
+		case "÷":
+			result = firstNum / secondNum;
+			break;
+		case "x":
+			result = firstNum * secondNum;
+			break;
+		case "-":
+			result = firstNum - secondNum;
+			break;
+		case "+":
+			result = firstNum + secondNum;
+			break;
+		case "x^":
+			result = Math.pow(firstNum, secondNum);
+			break;
+		default:
+			display.setText("Invalid symbol");
+			break;	
+		}
+		display.setText(String.valueOf(result));
 	}
 	
 	
