f7217035cf5825a5431285c85be5753c17e95996
octavianc96 <octavian.c96@gmail.com>
2017-03-13 02:24:29 +0200

    Implemented design and base functions

--- Lab 2/src/application/MainController.java
+++ Lab 2/src/application/MainController.java
@@ -1,5 +1,36 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class MainController {
+	
+	@FXML
+	private Label display;
+	private String output = "";
+	
+	@FXML
+	public void processNum (ActionEvent event) {
+		String value = ((Button)event.getSource()).getText();
+		display.setText(display.getText() + value);
+	}
+	
+	@FXML
+	public void processOperators (ActionEvent event) {
+		
+	}
+	
+	@FXML
+	public void processDecimal (ActionEvent event) {
+	
+	}
+	
+	@FXML
+	public void processEqual (ActionEvent event) {
+		
+	}
+	
+	
 
 }
