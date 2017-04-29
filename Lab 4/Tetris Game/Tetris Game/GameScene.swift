dd15b8cda28796cf1fe5a3aabab132be7254dae4
Octavian <octavian.c96@gmail.com>
2017-04-29 19:28:19 +0300

    Created Project

---
+++ Lab 4/Tetris Game/Tetris Game/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  Tetris Game
+//
+//  Created by Octavian Coroletchi on 4/29/17.
+//  Copyright © 2017 JokDev's Co. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
---
+++ Lab 4/Tetris Game/Tetris Game/GameViewController.swift
@@ -0,0 +1,27 @@
+//
+//  GameViewController.swift
+//  Tetris Game
+//
+//  Created by Octavian Coroletchi on 4/29/17.
+//  Copyright © 2017 JokDev's Co. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
